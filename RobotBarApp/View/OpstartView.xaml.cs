64127cdc2875861842dd40b4f85526861b4aa423
BaronVonBuron <baronvonburon@gmail.com>
2025-11-15 05:35:21 +0100

    Tilføjede alle admin views som placeholders, og opdaterede logo til menu icon

---
+++ RobotBarApp/View/OpstartView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace RobotBarApp.View
+{
+    public partial class OpstartView : UserControl
+    {
+        public OpstartView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RobotBarApp/View/TilfoejDrinkView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace RobotBarApp.View
+{
+    public partial class TilfoejDrinkView : UserControl
+    {
+        public TilfoejDrinkView()
+        {
+            InitializeComponent();
+        }
+    }
+}
