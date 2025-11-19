db7582bc04f468aa05928df7762204bf9c4f2f81
BaronVonBuron <baronvonburon@gmail.com>
2025-11-19 06:27:13 +0100

    tilføjet katalog + katalogitem som helper til den

---
+++ RobotBarApp/View/KatalogItemView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace RobotBarApp.View;
+
+public partial class KatalogItemView : UserControl
+{
+    public KatalogItemView()
+    {
+        InitializeComponent();
+    }
+}
