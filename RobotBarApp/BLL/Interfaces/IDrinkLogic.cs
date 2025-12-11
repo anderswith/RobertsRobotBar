38aae89d4885a7d7f966a3180b2a4da77ae62463
Anderswith <anderswith@hotmail.com>
2025-12-11 13:57:44 +0100

    clean up

--- RobotBarApp/BLL/Interfaces/IDrinkLogic.cs
+++ RobotBarApp/BLL/Interfaces/IDrinkLogic.cs
@@ -7,7 +7,7 @@ public interface IDrinkLogic
     IEnumerable<Drink> GetAllDrinks();
     Drink? GetDrinkById(Guid drinkId);
 
-    void AddDrink(string name, string image, bool IsMocktail, List<Guid> ingredientIds, List<string> scriptNames);
+    void AddDrink(string name, string image, bool isMocktail, List<Guid> ingredientIds, List<string> scriptNames);
     void DeleteDrink(Guid drinkId);
 
     void UpdateDrink(Guid drinkId, string name, string image, bool isMocktail, List<Guid> ingredientIds,
