8f4e37a94a276516ef1b26841851d69395865e09
Anderswith <anderswith@hotmail.com>
2025-12-17 15:07:48 +0100

    Katalogview done with filtering and deletion

---
+++ RobotBarApp/ViewModels/KatalogItemType.cs
@@ -0,0 +1,8 @@
+namespace RobotBarApp.ViewModels;
+
+public enum KatalogItemType
+{
+    Ingredient,
+    Drink,
+    Event
+}
--- RobotBarApp/ViewModels/KatalogViewModel.cs
+++ RobotBarApp/ViewModels/KatalogViewModel.cs
@@ -1,7 +1,280 @@
-namespace RobotBarApp.ViewModels
-{
-    public class KatalogViewModel : ViewModelBase
-    {
-    }
-}
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using RobotBarApp.BLL.Interfaces;
 
+namespace RobotBarApp.ViewModels;
+
+public class KatalogViewModel : ViewModelBase
+{
+    private readonly IDrinkLogic _drinkLogic;
+    private readonly IIngredientLogic _ingredientLogic;
+    private readonly IEventLogic _eventLogic;
+    
+    
+    
+    // Master list (never filtered)
+    private readonly List<KatalogItemViewModel> _allItems = new();
+
+    // Currently active filter
+    private KatalogItemType? _activeFilter = null;
+    
+    //filter options
+    private bool _isIngredientsSelected;
+    private bool _isDrinksSelected;
+    private bool _isEventsSelected;
+    private string _searchText = string.Empty;
+
+    public ObservableCollection<KatalogItemViewModel> CatalogItems { get; }
+        = new();
+
+    public ICommand FilterCommand { get; }
+
+    public KatalogViewModel(
+        IDrinkLogic drinkLogic,
+        IIngredientLogic ingredientLogic,
+        IEventLogic eventLogic)
+    {
+        _drinkLogic = drinkLogic;
+        _ingredientLogic = ingredientLogic;
+        _eventLogic = eventLogic;
+
+        FilterCommand = new RelayCommand(ChangeFilter);
+
+        LoadAll();
+    }
+    
+
+    private void LoadAll()
+    {
+        CatalogItems.Clear();
+        _allItems.Clear();
+
+        LoadDrinks();
+        LoadIngredients();
+        LoadEvents();
+
+        ApplyFilter();
+    }
+
+    private void LoadDrinks()
+    {
+        foreach (var drink in _drinkLogic.GetAllDrinks())
+        {
+            _allItems.Add(
+                CreateItem(
+                    drink.DrinkId,
+                    drink.Name,
+                    drink.Image,
+                    KatalogItemType.Drink));
+        }
+    }
+
+    private void LoadIngredients()
+    {
+        foreach (var ingredient in _ingredientLogic.GetAllIngredients())
+        {
+            _allItems.Add(
+                CreateItem(
+                    ingredient.IngredientId,
+                    ingredient.Name,
+                    ingredient.Image,
+                    KatalogItemType.Ingredient));
+        }
+    }
+
+    private void LoadEvents()
+    {
+        foreach (var ev in _eventLogic.GetAllEvents())
+        {
+            _allItems.Add(
+                CreateItem(
+                    ev.EventId,
+                    ev.Name,
+                    ev.Image,
+                    KatalogItemType.Event));
+        }
+    }
+
+
+
+    private KatalogItemViewModel CreateItem(
+        Guid id,
+        string name,
+        string imagePath,
+        KatalogItemType type)
+    {
+        return new KatalogItemViewModel(
+            id,
+            name,
+            LoadImage(imagePath),
+            type,
+            DeleteItem,
+            EditItem);
+    }
+
+
+    public bool IsIngredientsSelected
+    {
+        get => _isIngredientsSelected;
+        set
+        {
+            if (_isIngredientsSelected == value) return;
+            _isIngredientsSelected = value;
+            OnPropertyChanged();
+        }
+    }
+    public bool IsEventsSelected
+    {
+        get => _isEventsSelected;
+        set
+        {
+            if (_isEventsSelected == value) return;
+            _isEventsSelected = value;
+            OnPropertyChanged();
+        }
+    }
+    public bool IsDrinksSelected
+    {
+        get => _isDrinksSelected;
+        set
+        {
+            if (_isDrinksSelected == value) return;
+            _isDrinksSelected = value;
+            OnPropertyChanged();
+        }
+    }
+    
+    private void ChangeFilter(object? parameter)
+    {
+        if (parameter is not string filter)
+            return;
+        IsIngredientsSelected = false;
+        IsDrinksSelected = false;
+        IsEventsSelected = false;
+
+        KatalogItemType? selectedFilter = null;
+
+        switch (filter)
+        {
+            case "Ingredients":
+                IsIngredientsSelected = true;
+                selectedFilter = KatalogItemType.Ingredient;
+                break;
+
+            case "Drinks":
+                IsDrinksSelected = true;
+                selectedFilter = KatalogItemType.Drink;
+                break;
+
+            case "Events":
+                IsEventsSelected = true;
+                selectedFilter = KatalogItemType.Event;
+                break;
+        }
+
+        // Toggle off behavior:
+        _activeFilter = _activeFilter == selectedFilter
+            ? null
+            : selectedFilter;
+
+        ApplyFilter();
+    }
+
+    private void DeleteItem(Guid id, KatalogItemType type)
+    {
+        switch (type)
+        {
+            case KatalogItemType.Drink:
+                _drinkLogic.DeleteDrink(id);
+                break;
+
+            case KatalogItemType.Ingredient:
+                _ingredientLogic.DeleteIngredient(id);
+                break;
+
+            case KatalogItemType.Event:
+                _eventLogic.DeleteEvent(id);
+                break;
+        }
+
+        RemoveItem(id, type);
+    }
+
+    private void EditItem(Guid id, KatalogItemType type)
+    {
+        // Implement later
+    }
+
+    public string SearchText
+    {
+        get => _searchText;
+        set
+        {
+            if (_searchText == value)
+                return;
+
+            _searchText = value;
+            OnPropertyChanged();
+            ApplyFilter(); // realtime filtering
+        }
+    }
+
+    private void ApplyFilter()
+    {
+        CatalogItems.Clear();
+
+        IEnumerable<KatalogItemViewModel> items = _allItems;
+
+        if (_activeFilter.HasValue)
+        {
+            items = items.Where(i => i.ItemType == _activeFilter.Value);
+        }
+        if (!string.IsNullOrWhiteSpace(SearchText))
+        {
+            items = items.Where(i =>
+                i.Name.StartsWith(
+                    SearchText,
+                    StringComparison.OrdinalIgnoreCase));
+        }
+
+        foreach (var item in items)
+            CatalogItems.Add(item);
+    }
+
+
+
+    
+    private void RemoveItem(Guid id, KatalogItemType type)
+    {
+        var item = _allItems.FirstOrDefault(i => i.Id == id && i.ItemType == type);
+        if (item != null)
+        {
+            _allItems.Remove(item);
+            CatalogItems.Remove(item);
+        }
+    }
+
+    private ImageSource LoadImage(string path)
+    {
+        Uri uri = Path.IsPathRooted(path)
+            ? new Uri(path, UriKind.Absolute)
+            : new Uri(path, UriKind.Relative);
+
+        var image = new BitmapImage();
+        image.BeginInit();
+        image.UriSource = uri;
+        image.CacheOption = BitmapCacheOption.OnLoad;
+        image.EndInit();
+        image.Freeze();
+
+        return image;
+    }
+
+
+}
