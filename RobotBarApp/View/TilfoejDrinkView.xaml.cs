9c0bb3fe1a8ab791304dd18b7b5a42fd5757f479
andhot01 <andhot01@easv365.dk>
2026-01-02 14:08:36 +0100

    Banned danish characters from scripts input

--- RobotBarApp/View/TilfoejDrinkView.xaml.cs
+++ RobotBarApp/View/TilfoejDrinkView.xaml.cs
@@ -1,12 +1,41 @@
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace RobotBarApp.View
 {
-    public partial class TilfoejDrinkView : UserControl
+    public partial class TilfoejDrinkView
     {
+        private static readonly char[] DisallowedScriptChars = { 'æ', 'ø', 'å', 'Æ', 'Ø', 'Å' };
+
         public TilfoejDrinkView()
         {
             InitializeComponent();
+        }
+
+        private static bool ContainsDisallowedScriptChars(string? text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return false;
+
+            return text.IndexOfAny(DisallowedScriptChars) >= 0;
+        }
+
+        private void ScriptTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            // Blocks typing disallowed characters. MaxLength is handled by the TextBox itself.
+            if (ContainsDisallowedScriptChars(e.Text))
+                e.Handled = true;
+        }
+
+        private void ScriptTextBox_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            if (!e.SourceDataObject.GetDataPresent(DataFormats.UnicodeText, true))
+                return;
+
+            var pastedText = e.SourceDataObject.GetData(DataFormats.UnicodeText) as string;
+            if (ContainsDisallowedScriptChars(pastedText))
+                e.CancelCommand();
         }
     }
 }
